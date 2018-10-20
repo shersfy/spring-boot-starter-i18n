ba304e64038c964bc03c65651bfd540aec257dd5
pengyang <pengyang@300.cn>
2018-10-21 03:25:39 +0800

    add i18n model

--- src/main/java/org/shersfy/i18n/I18nMessages.java
+++ src/main/java/org/shersfy/i18n/I18nMessages.java
@@ -4,6 +4,10 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
+/**
+ * 注入后即可使用
+ *
+ */
 public class I18nMessages {
 	
 	private Map<String, I18nProperties> i18n;
@@ -15,6 +19,15 @@ public class I18nMessages {
 
 	public Map<String, I18nProperties> getI18n() {
 		return i18n;
+	}
+	
+	public I18nProperties getI18n(String language) {
+		Locale locale = Locale.CHINA;
+		if("en".equalsIgnoreCase(language)
+				|| "en_US".equalsIgnoreCase(language)) {
+			locale = Locale.US;
+		}
+		return getI18n(locale.getLanguage(), locale.getCountry());
 	}
 	
 	public I18nProperties getI18n(String language, String country) {
