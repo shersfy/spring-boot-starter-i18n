8e67a901fa1465290fb1c16432a3fe633da7d63d
pengyang <pengyang@300.cn>
2018-10-21 20:10:22 +0800

    抽象化

---
+++ src/main/java/org/shersfy/i18n/I18nResponseAdvice.java
@@ -0,0 +1,42 @@
+package org.shersfy.i18n;
+
+import org.shersfy.i18n.beans.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+public abstract class I18nResponseAdvice implements ResponseBodyAdvice<Object>{
+
+	@Autowired
+	private I18nMessages i18n;
+
+	@Override
+	public boolean supports(MethodParameter returnType, 
+			Class<? extends HttpMessageConverter<?>> converterType) {
+		return true;
+	}
+
+	@Override
+	public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+			Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request,
+					ServerHttpResponse response) {
+		if(!(body instanceof Result)) {
+			return body;
+		}
+		
+		Result res = (Result) body;
+		if(res.getModel() instanceof I18nModel) {
+			I18nModel model = (I18nModel) res.getModel();
+			String lang = request.getHeaders().getFirst("lang");
+			String msg  = i18n.getI18n(lang).getProperty(model.getKey(), model.getArgs());
+			res.setMsg(msg);
+		}
+		
+		return res;
+	}
+	
+}
